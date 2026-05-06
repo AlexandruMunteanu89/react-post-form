028c7a9269cedc32492a805909f557fc2ca191d7
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-05-06 18:01:21 +0200

    Creato la funzione handleSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ const NuovoForm = () => {
   const [body, setBody] = useState('');
   const [publicPost, setPublicPost] = useState(false);
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {
+      author,
+      title,
+      body,
+      public: publicPost,
+    };
+    console.log(newPost);
+  };
   return (
     <>
       
@@ -14,4 +23,4 @@ const NuovoForm = () => {
   )
 }
 
-export default App;
+export default NuovoForm;
